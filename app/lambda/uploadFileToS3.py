9f405f658a3812eead655decc0b9d786270d5c30
Anurag Yadav <59284397+AnuragYadav171@users.noreply.github.com>
2020-01-03 23:07:39 +0530

    Update uploadFileToS3.py

--- app/lambda/uploadFileToS3.py
+++ app/lambda/uploadFileToS3.py
@@ -2,15 +2,15 @@ from flask import Flask, escape, request, render_template
 import boto3
 from botocore.client import Config
 
-app = Flask(__name__, template_folder='../template')
+app = Flask(__name__, template_folder='template')
 
-ACCESS_KEY_ID = 'AKIAJM5DQ3KG74XDASHQ'
-ACCESS_SECRET_KEY = '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'
+ACCESS_KEY_ID = 'AKIAI3HGHAES6K5SNOQQ'
+ACCESS_SECRET_KEY = '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'
 BUCKET_NAME = 'anurag1306171a'
 
 @app.route('/')
 def index():
-    return render_template('../index.html')
+    return render_template('index.html')
 
 # a route where we will display a welcome message via an HTML template
 @app.route("/uploader", methods=['GET','POST'])
@@ -21,7 +21,7 @@ def upload():
 
         if file :
             FILE_NAME = file.filename
-            data = open(FILE_NAME, 'rb')
+            # data = open(FILE_NAME, 'rb')
             # S3 Connect
             s3 = boto3.resource(
                 's3',
@@ -31,10 +31,11 @@ def upload():
             )
 
             # Image Uploaded
-            s3.Bucket(BUCKET_NAME).put_object(Key=FILE_NAME, Body=data, ACL='public-read')
+            # s3.Bucket(BUCKET_NAME).put_object(Key=FILE_NAME, Body=data, ACL='public-read')
+            s3.Bucket(BUCKET_NAME).put_object(Key=FILE_NAME, ACL='public-read')
 
             print ("Done")
-            return render_template('../index.html', message=FILE_NAME + ' -> Uploaded to S3 *(' + BUCKET_NAME + ')* - AWS !');
+            return render_template('index.html', message=FILE_NAME + ' -> Uploaded to S3 *(' + BUCKET_NAME + ')* - AWS !');
 
 # run the application
 if __name__ == "__main__":
