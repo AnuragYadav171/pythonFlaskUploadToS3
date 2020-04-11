f2003b8abb3c723672637e98b81cf38efdb3930b
Anurag Yadav <59284397+AnuragYadav171@users.noreply.github.com>
2020-04-11 19:55:15 +0530

    Update runScript.sh

--- runScript.sh
+++ runScript.sh
@@ -5,4 +5,4 @@ whoami
 cd /var/lib/jenkins/workspace/GitRepoProj/app/lambda
 pwd
 kill -9 `ps -ef | grep -v grep | grep "lambda" | awk '{print $2}'`
-python uploadFileToS3.py
+nohup python uploadFileToS3.py &
